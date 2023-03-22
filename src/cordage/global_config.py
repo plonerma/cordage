572c5e2a181a92efe22f5207b93c747b1036af4f
Max Ploner <plonerma@maxploner.de>
2023-03-22 16:06:25 +0100

    Fixed default global config, added validation of format strings

--- src/cordage/global_config.py
+++ src/cordage/global_config.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
+from typing import Any, Dict
 
 
 @dataclass
@@ -28,7 +30,7 @@ class GlobalConfig:
     base_output_dir: Path = Path("results")
 
     experiment_id_format: str = "{start_time:%Y-%m-%d_%H-%M-%S}"
-    output_dir_format: str = "{experiment_id:%Y-%m}/{trial_id}"
+    output_dir_format: str = "{start_time:%Y-%m}/{experiment_id}"
 
     series_spec_key = "__series__"
 
@@ -37,3 +39,16 @@ class GlobalConfig:
     file_tree: FileTreeConfig = FileTreeConfig()
 
     central_metadata: CentralMetadataConfig = CentralMetadataConfig()
+
+    def __post_init__(self):
+        super().__init__()
+
+        self.validate_format_strings()
+
+    def validate_format_strings(self):
+        # check the format strings
+        dummy_metadata: Dict[str, Any] = dict(start_time=datetime.now())
+        self.experiment_id_format.format(**dummy_metadata)
+
+        dummy_metadata["experiment_id"] = "experiment_id"
+        self.output_dir_format.format(**dummy_metadata)
--- tests/test_global_config.py
+++ tests/test_global_config.py
@@ -1,6 +1,9 @@
 import dataclasses
 import json
 
+import pytest
+
+from cordage import GlobalConfig
 from cordage.util import to_dict
 
 
@@ -12,3 +15,23 @@ def test_global_config(global_config):
     json.dumps(to_dict(global_config.central_metadata))
 
     json.dumps(to_dict(global_config))
+
+
+def test_format_string_validation(global_config):
+    # This should be valid
+    _ = GlobalConfig()
+
+    with pytest.raises(KeyError):
+        _ = dataclasses.replace(global_config, experiment_id_format="{non_existing_field}")
+
+    with pytest.raises(KeyError):
+        _ = dataclasses.replace(global_config, experiment_id_format="{experiment_id}")
+
+    # Output dir should contain the experiment id
+    _ = dataclasses.replace(global_config, output_dir_format="{experiment_id}")
+
+    with pytest.raises(KeyError):
+        _ = dataclasses.replace(global_config, output_dir_format="{non_existing_field}")
+
+    with pytest.raises(ValueError):
+        _ = dataclasses.replace(global_config, output_dir_format="{experiment_id:%Y}")
